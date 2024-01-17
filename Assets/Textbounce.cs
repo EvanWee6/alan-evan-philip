af72b4fe24d9e5859e8663c72a62d212cd81ffdb
alanjsuratos <125233948+alanjsuratos@users.noreply.github.com>
2024-01-17 13:08:30 -0500

    Title

---
+++ Assets/Textbounce.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceText : MonoBehaviour
+{
+    public float bounceHeight = 0.5f;
+    public float bounceSpeed = 2.0f;
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        float newY = startPos.y + Mathf.Sin(Time.time * bounceSpeed) * bounceHeight;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+}
